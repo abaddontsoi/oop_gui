69b7be98e9748c7a2ad7befa76e71d1b2f1b6d16
Friendly_abaddon <67108399+abaddontsoi@users.noreply.github.com>
2021-11-04 10:50:32 +0800

    add demo

---
+++ GUIDemo1.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUIDemo1 {
+    
+    public static void main(String[] args) {
+
+        JFrame demo = new JFrame("tittle?");
+        JCheckBox checkBox = new JCheckBox("new checkbox");
+        JRadioButton radioButton = new JRadioButton("radio");
+        JButton buttonObject = new JButton("button");
+        JLabel labelObject = new JLabel("label");
+        JTextArea textObject = new JTextArea("text");            
+
+        demo.setSize(400, 300);
+        demo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        demo.getContentPane().add(BorderLayout.EAST, checkBox);
+        demo.getContentPane().add(BorderLayout.WEST, radioButton);
+        demo.getContentPane().add(BorderLayout.SOUTH, buttonObject);
+        demo.getContentPane().add(BorderLayout.NORTH , labelObject);
+        demo.getContentPane().add(BorderLayout.CENTER, textObject);
+
+        demo.setVisible(true);
+        
+    }
+}
